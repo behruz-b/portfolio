8fb2197fa2e772deb8d2997e97704dbe2c7d9ee4
Behruz <behruz.ravshonbekovich@gmail.com>
2020-02-07 09:50:58 +0500

    initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import com.typesafe.scalalogging.LazyLogging
+import javax.inject._
+import play.api.mvc._
+import views.html._
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents,
+                              indexTemplate: index) extends BaseController with LazyLogging {
+
+  /**
+   * Create an Action to render an HTML page with a welcome message.
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index = Action {
+    Ok(indexTemplate())
+  }
+
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,7 @@
+logLevel := Level.Warn
+
+resolvers += "Typesafe repository" at "https://repo.typesafe.com/typesafe/releases/"
+
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.4")
+addSbtPlugin("com.typesafe.sbt" % "sbt-coffeescript" % "1.0.2")
+addSbtPlugin("com.typesafe.sbt" % "sbt-less" % "1.1.2")
