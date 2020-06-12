f9076ddf3a5434d13c460b6af647d2a5b262be16
yoshhacker <shomoyxhacker007@mail.ru>
2020-06-12 20:29:18 +0500

    1 bug fixed

--- public/javascripts/agency.js
+++ public/javascripts/agency.js
@@ -25,3 +25,15 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+//
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("nav-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
