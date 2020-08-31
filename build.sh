850a71bcbee6bc36cf4f44ed32d74896e24799dc
Prince <prince777_98@mail.ru>
2020-08-31 15:23:41 +0500

    build.sh changed

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker pull postgres:11
+docker pull postgres:latest
 
 docker build -t portfolio ./deploy/
 
-docker run --name portfolio-db -p 5432:5432 -e 'POSTGRES_PASSWORD=maftunbek1998' -e 'POSTGRES_USER=prince' -d postgres:11
+docker run --name portfolio-db -p 5432:5432 -e 'POSTGRES_PASSWORD=maftunbek1998' -e 'POSTGRES_USER=prince' -d postgres:latest
 
 sleep 3s
 
@@ -12,4 +12,4 @@ docker exec -it portfolio-db psql -U prince -c "create database portfolio"
 
 cat ./conf/database/db.sql | docker exec -i portfolio-db psql -U prince -d portfolio
 
-source deploy.sh
+. ./deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 sbt dist
 
-docker run --rm -ti -v $PWD/target/universal/portfolio-1.0.zip:/opt/portfolio-1.0.zip -p 8080:9000 --link=portfolio-db:postgres-host portfolio:latest
+docker run --rm -d -ti -v $PWD/target/universal/portfolio-1.0.zip:/opt/portfolio-1.0.zip -p 8080:9000 --link=portfolio-db:postgres-host portfolio:latest
